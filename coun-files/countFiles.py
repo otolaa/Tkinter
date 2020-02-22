4b942d38b53b13a1384a30d670dda2fb8377f16c
otolaa <kalabunga82@gmail.com>
2020-02-22 15:43:01 +0200

    text return

--- coun-files/countFiles.py
+++ coun-files/countFiles.py
@@ -14,9 +14,23 @@ def fexpansion(path):
             return True
     return False
 
+# возвращает известную кодировку файла
+def correct_encoding(path):
+    encodingList = ['utf-8', 'windows-1251', 'cp500', 'utf-16', 'GBK', 'ASCII', 'US-ASCII', 'Big5']
+    encodingCod = ''
+    for enc in encodingList:
+        try:
+            open(path, encoding = enc).read()
+        except (UnicodeDecodeError, LookupError):
+            pass
+        else:
+            encodingCod = enc
+            # print('Кодировка файла -- ' + encodingCod + ' ' + path)
+            break
+    return encodingCod
 
 def main():
-    src = "d:\\OSPanel\\domains\\westpower\\infosystems\\local\\php_interface\\"
+    src = "d:\\OSPanel\\domains\\westpower\\infosystems\\local\\templates\\infosystems\\"
     farr = []
     sm = 0
     skb = 0
@@ -31,15 +45,16 @@ def main():
             # размер файла в kбайтах, округление до второго знака
             kb = round(os.path.getsize(path)/1024, 2)
             # если проникнуться дзеном пайтона, то подсчитать количество строк в файле можно так:
-            # генератор списка с условием, выбираем строки только те которые больше 3-x символов
-            s = sum(1 for line in open(path, 'r', encoding='utf-8') if len(line) > 3)           
-            p(path, kb, s)
+            # генератор списка с условием, выбираем строки только те которые больше 5-ти символов, '<?php'
+            encodingCod = correct_encoding(path) # определаем кодировку файла
+            s = sum(1 for line in open(path, 'r', encoding = encodingCod) if len(line) > 5)           
+            p(path, encodingCod, str(kb) + ' kB', s)
             # добавляет записи в список, для подсчета количества файлов
             farr.append(path)
             sm += s  # количество строк, += сложение и присваивание
             skb += kb # количество кбайт, += сложение и присваивание
     # возвращает количество записей в списке, килобайт в файлах, количество строк
-    p(len(farr), round(skb, 2), sm)
+    p('Количество файлов: ' + str(len(farr)), 'Размер: ' + str(round(skb, 2)) + ' kB', 'Всего строк кода: ' + str(sm))
     # p(farr)
 
 if __name__ == '__main__':
